6be92b23e09419ca9bfa1f91125942318d742165
alpha02x <nsnt80@gmail.com>
2023-05-25 19:42:11 +0500

    proper persistence

--- src/components/AnomaliesTable/Calculator.tsx
+++ src/components/AnomaliesTable/Calculator.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TableColumn } from "../../entities/TableColumn";
+import { AnomalyDefinitons } from "../../entities/Constants";
 
 export type CalculatorProps = {
 	tableColumns: TableColumn[];
@@ -11,11 +12,22 @@ export class Calculator extends React.Component<CalculatorProps> {
 		let income: number = 0;
 		income = columns
 			.filter((col) => col.charsPassed.some((x) => x === char))
-			.reduce((income, currentColumn) => income + currentColumn.getPrice() / currentColumn.charsPassed.length, income);
+			.reduce((income, currentColumn) => income + this.getPrice(currentColumn) / currentColumn.charsPassed.length, income);
 		return income.toLocaleString("ru-RU", {
 			minimumFractionDigits: 2,
 			maximumFractionDigits: 2,
 		});
+	}
+
+	getPrice(column: TableColumn): number {
+		let definition = AnomalyDefinitons.get(column.anomalyType);
+		let result: number = 0;
+
+		if (column.additionalRatKilled) result += definition?.priceWithAdditionalShips ?? 0;
+		else result += definition?.basePrice ?? 0;
+
+		if (column.drifterKilled) result += 300000000;
+		return result;
 	}
 
 	render(): React.ReactNode {
@@ -23,7 +35,7 @@ export class Calculator extends React.Component<CalculatorProps> {
 			<table className="calculator">
 				{this.props.chars.map((char) => (
 					<tr>
-						<td>{char}</td>
+						<td className="calculatorFirstColumn">{char}</td>
 						<td>{this.calculateForChar(char, this.props.tableColumns) + " ISK"}</td>
 					</tr>
 				))}
