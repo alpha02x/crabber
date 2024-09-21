301d384226a7768e15a9f98b32d95e4fe6a659ce
alpha02x <nsnt80@gmail.com>
2024-09-21 13:32:11 +0500

    init 2

--- src/components/AnomaliesTable/AnomaliesTable.tsx
+++ src/components/AnomaliesTable/AnomaliesTable.tsx
@@ -5,6 +5,8 @@ import { AnomalyAdditionControl } from "./AnomalyAdditionControl";
 import { AnomalyDefinitons } from "../../entities/Constants";
 import { TableColumn } from "../../entities/TableColumn";
 import "./AnomaliesTable.css";
+import { Calculator } from "./Calculator";
+import { ColumnHeading } from "./ColumnHeading";
 
 type AnomaliesTableState = {
 	tableColumns: TableColumn[];
@@ -16,6 +18,31 @@ export class AnomaliesTable extends React.Component {
 		tableColumns: [],
 		chars: [],
 	};
+
+	removeColumn(columnName: string) {
+		this.setStateInternal({
+			chars: this.state.chars,
+			tableColumns: this.state.tableColumns.filter((x) => x.name !== columnName),
+		});
+	}
+
+	setStateInternal(state: AnomaliesTableState) {
+		this.setState(state);
+	}
+
+	changeAddRat(columnName: string) {
+		let newState = this.state;
+		let index = this.state.tableColumns.findIndex((x) => x.name === columnName);
+		newState.tableColumns[index].additionalRatKilled = !newState.tableColumns[index].additionalRatKilled;
+		this.setStateInternal(newState);
+	}
+
+	changeDrifter(columnName: string) {
+		let newState = this.state;
+		let index = this.state.tableColumns.findIndex((x) => x.name === columnName);
+		newState.tableColumns[index].drifterKilled = !newState.tableColumns[index].drifterKilled;
+		this.setStateInternal(newState);
+	}
 
 	setCharStatusForAnomaly(char: string, anomalyName: string, passed: boolean) {
 		let newState = this.state;
@@ -32,14 +59,14 @@ export class AnomaliesTable extends React.Component {
 			}
 		}
 
-		this.setState(newState);
+		this.setStateInternal(newState);
 	}
 
 	addChar(char: string) {
 		let newState = this.state;
 		if (!this.state.chars.some((x) => x === char)) {
 			newState.chars.push(char);
-			this.setState(newState);
+			this.setStateInternal(newState);
 		}
 	}
 
@@ -47,8 +74,9 @@ export class AnomaliesTable extends React.Component {
 		let newState = this.state;
 		let anomalyDefinition = AnomalyDefinitons.get(type);
 		let alreadyPresentAnomsOfType = this.state.tableColumns.filter((col) => col.anomalyType === type).length;
-		newState.tableColumns.push(new TableColumn((anomalyDefinition?.short ?? "unknown") + (alreadyPresentAnomsOfType + 1), type));
-		this.setState(newState);
+		let columnName = (anomalyDefinition?.short ?? "unknown") + (alreadyPresentAnomsOfType + 1);
+		newState.tableColumns.push(new TableColumn(columnName, type));
+		this.setStateInternal(newState);
 	}
 
 	render() {
@@ -59,7 +87,14 @@ export class AnomaliesTable extends React.Component {
 						<tr className="tableHeaderRow">
 							<th>Персы</th>
 							{this.state.tableColumns.map((column) => (
-								<th className="tableColumnHeaderAnomaly">{column.name}</th>
+								<th className="tableColumnHeaderAnomaly">
+									<ColumnHeading
+										removeColumn={this.removeColumn.bind(this)}
+										changeAddRat={this.changeAddRat.bind(this)}
+										changeDrifter={this.changeDrifter.bind(this)}
+										tableColumn={column}
+									/>
+								</th>
 							))}
 							<th>
 								<span className="anomalyAdditionControlContainer">
@@ -72,7 +107,12 @@ export class AnomaliesTable extends React.Component {
 								<td>{char}</td>
 								{this.state.tableColumns.map((column) => (
 									<td>
-										<AnomalyPassedCheckBox anomaliesTable={this} char={char} anomalyName={column.name} />
+										<AnomalyPassedCheckBox
+											checked={column.charsPassed.some((passed) => passed === char)}
+											anomaliesTable={this}
+											char={char}
+											anomalyName={column.name}
+										/>
 									</td>
 								))}
 							</tr>
@@ -84,23 +124,12 @@ export class AnomaliesTable extends React.Component {
 						</tr>
 					</table>
 				</div>
-                <div className="calculatorContainer">
-
-                </div>
+				<div className="calculatorContainer">
+					<Calculator tableColumns={this.state.tableColumns} chars={this.state.chars} />
+				</div>
 			</div>
 		);
 	}
 }
 
 export default AnomaliesTable;
-
-
-type CalculatorProps = {
-	tableState: AnomaliesTableState;
-};
-
-class Calculator extends React.Component<CalculatorProps> {
-render(): React.ReactNode {
-    
-}
-}
--- src/entities/TableColumn.tsx
+++ src/entities/TableColumn.tsx
@@ -1,11 +1,26 @@
+import { AnomalyDefinitons } from "./Constants";
+
 export class TableColumn {
 	name: string;
 	anomalyType: string;
 	charsPassed: string[];
+	drifterKilled: boolean = false;
+	additionalRatKilled: boolean = false;
 
 	constructor(name: string, anomalyType: string) {
 		this.name = name;
 		this.anomalyType = anomalyType;
 		this.charsPassed = [];
+	}
+
+	getPrice(): number {
+		let definition = AnomalyDefinitons.get(this.anomalyType);
+		let result: number = 0;
+
+		if (this.additionalRatKilled) result += definition?.priceWithAdditionalShips ?? 0;
+		else result += definition?.basePrice ?? 0;
+
+		if (this.drifterKilled) result += 300000000;
+		return result;
 	}
 }
