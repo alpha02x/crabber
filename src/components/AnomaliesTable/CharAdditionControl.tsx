f82b0119081030318bb4a6d944d3fb6cf41c03ec
alpha02x <nsnt80@gmail.com>
2024-09-21 13:32:15 +0500

    add char addition button

--- src/components/AnomaliesTable/CharAdditionControl.tsx
+++ src/components/AnomaliesTable/CharAdditionControl.tsx
@@ -1,23 +1,47 @@
-import React, { KeyboardEvent } from "react";
+import React, { ChangeEvent, KeyboardEvent } from "react";
 
 type CharAdditionControlProps = {
 	addChar: (char: string) => void;
 };
 
-export class CharAdditionControl extends React.Component<CharAdditionControlProps> {
+type CharAdditionControlState = {
+	charName: string;
+};
+
+export class CharAdditionControl extends React.Component<CharAdditionControlProps, CharAdditionControlState> {
+	state = { charName: "" };
 	onKeyDown(event: KeyboardEvent<HTMLInputElement>) {
 		if (event.key === "Enter") {
-			if (event.currentTarget.value === "") return;
-			this.addNewChar(event.currentTarget.value);
-			event.currentTarget.value = "";
+			this.addNewChar(this.state.charName);
 		}
 	}
 
 	addNewChar(char: string) {
+		if (char === "") return;
 		this.props.addChar(char);
+		(document.getElementById("charAdditionControl") as HTMLTextAreaElement).value = "";
+	}
+
+	onChange(event: ChangeEvent<HTMLInputElement>) {
+		this.setState({ charName: event.currentTarget.value });
 	}
 
 	render(): React.ReactNode {
-		return <input className="charAdditionControl" placeholder="Введи имя персонажа" onKeyDown={this.onKeyDown.bind(this)} type="text"></input>;
+		return (
+			<div className="charAdditionControl">
+				{" "}
+				<input
+					id="charAdditionControl"
+					className="charAdditionControl"
+					placeholder="Введи имя персонажа"
+					onChange={this.onChange.bind(this)}
+					onKeyDown={this.onKeyDown.bind(this)}
+					type="text"
+				></input>
+				<button onClick={() => this.addNewChar(this.state.charName)} className="addCharButton">
+					▸
+				</button>
+			</div>
+		);
 	}
 }
