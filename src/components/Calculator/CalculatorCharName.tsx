d2bfee970608dd3efc37648a90aad62127e6da13
alpha02x <nsnt80@gmail.com>
2024-09-21 13:32:16 +0500

    fix coefficients readdition bug

--- src/components/Calculator/CalculatorCharName.tsx
+++ src/components/Calculator/CalculatorCharName.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent } from "react";
 
 export type CalculatorCharNameProps = {
-	char: string;
+	char: [name: string, coef: number];
 	changeCoefficient: (char: string, coefficient: number) => void;
 };
 
@@ -9,7 +9,7 @@ export class CalculatorCharName extends React.Component<CalculatorCharNameProps>
 	onChange(event: ChangeEvent<HTMLInputElement>) {
 		let parsedPercentage = Number.parseFloat(event.currentTarget.value);
 		let coefficient = Number.isNaN(parsedPercentage) ? 1 : 1 - (parsedPercentage > 100 ? 100 : parsedPercentage) / 100;
-		this.props.changeCoefficient(this.props.char, coefficient);
+		this.props.changeCoefficient(this.props.char[0], coefficient);
 	}
 
 	render(): React.ReactNode {
@@ -17,9 +17,20 @@ export class CalculatorCharName extends React.Component<CalculatorCharNameProps>
 			<div>
 				<div className="coefficientContainer">
 					<div className="coefficientInputContainer">
-						-{<input maxLength={3} className="coefficientInput" placeholder="0" onChange={this.onChange.bind(this)} type="text" />}%
+						-
+						{
+							<input
+								maxLength={3}
+								className="coefficientInput"
+								placeholder="0"
+								onChange={this.onChange.bind(this)}
+								type="text"
+								value={(100 - this.props.char[1] * 100).toString()}
+							/>
+						}
+						%
 					</div>
-					<div className="charNameLabel">{this.props.char}</div>
+					<div className="charNameLabel">{this.props.char[0]}</div>
 				</div>
 			</div>
 		);
