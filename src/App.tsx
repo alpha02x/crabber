6c9b0aa4c2d05eae79845c9e6a3c8618555de822
alpha02x <nsnt80@gmail.com>
2024-09-21 13:32:18 +0500

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+import React from "react";
 import "./App.css";
 import AnomaliesTable from "./components/AnomaliesTable/AnomaliesTable";
+import { DarkModeButton } from "./components/DarkModeButton/DarkModeButton";
 
-function App() {
-	return <AnomaliesTable />;
+type AppState = {
+	darkTheme: boolean;
+};
+
+export class App extends React.Component {
+	state: AppState = JSON.parse(
+		localStorage.getItem("dark-theme") ??
+			JSON.stringify({
+				darkTheme: false,
+			})
+	);
+
+	enableDarkTheme() {
+		let newState = { darkTheme: true };
+		this.setState(newState);
+		localStorage.setItem("dark-theme", JSON.stringify(newState));
+	}
+
+	disableDarkTheme() {
+		let newState = { darkTheme: false };
+		this.setState(newState);
+		localStorage.setItem("dark-theme", JSON.stringify(newState));
+	}
+
+	render(): React.ReactNode {
+		return [
+			<AnomaliesTable darkTheme={this.state.darkTheme} />,
+			<DarkModeButton
+				darkTheme={this.state.darkTheme}
+				disableDarkTheme={this.disableDarkTheme.bind(this)}
+				enableDarkTheme={this.enableDarkTheme.bind(this)}
+			/>,
+		];
+	}
 }
 
 export default App;
